65e68fad49b25e1f943a3472a33fb478dcbeecc2
kevin <judoka225@gmail.com>
2025-10-14 21:17:03 +0200

    Parte1-Introduccion

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css';
+import logoBatoi from '/logoBatoi.png';
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${logoBatoi}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Hello BatoiBooks!</h1>
+`
