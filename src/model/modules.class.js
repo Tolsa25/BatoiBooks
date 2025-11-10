48f6fb2c89b846a066d7bbe4e2b51a3fd1a1b816
kevin <judoka225@gmail.com>
2025-11-10 18:45:12 +0100

    Errores de la rama persistencia solucionados

--- src/model/modules.class.js
+++ src/model/modules.class.js
@@ -1,5 +1,5 @@
 import Module from './module.class';
-import { getDBModules, addDBModule, removeDBModule, changeDBModule } from '../services/modules.api.js'; 
+import { getDBModules } from '../services/modules.api.js'; 
 
 
 class Modules {
@@ -10,46 +10,6 @@ class Modules {
     async populate() {
         const dataInicial = await getDBModules();
         this.data = dataInicial.map(moduleData => new Module(moduleData));
-    }
-
-    async addModule(moduleData) {
-        if (!moduleData.code) {
-            throw new Error("No se puede añadir el módulo: la propiedad 'code' es obligatoria.");
-        }
-        
-        const createdModule = await addDBModule(moduleData);
-
-        const newModuleInstance = new Module(createdModule);
-
-        this.data.push(newModuleInstance);
-        return newModuleInstance;
-    }
-
-    async removeModule(code) {
-        await removeDBModule(code);
-        
-        const index = this.data.findIndex(module => module.code === code);
-
-        if (index === -1) {
-            throw new Error(`Módulo con código "${code}" no encontrado localmente.`);
-        }
-
-        this.data.splice(index, 1);
-    }
-
-    async changeModule(moduleData) {
-        const updatedModule = await changeDBModule(moduleData);
-        
-        const code = moduleData.code;
-        const index = this.data.findIndex(module => module.code === code);
-
-        if (index === -1) {
-            throw new Error(`Módulo con código "${code}" no encontrado localmente`);
-        }
-
-        this.data[index] = new Module(updatedModule);
-
-        return this.data[index];
     }
 
     toString() {
